ba9c9ffb653bd9fdeedbd49400dedbe32f31e984
Eldelshell <alejandroayuso@gmail.com>
2018-01-27 11:12:14 +0100

    Uncomment to test only one file

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,11 @@ module.exports = {
     verbose: false,
     testMatch: [
         "**/__tests__/**/*.js"
+        // "**/__tests__/**/units/distance/Metric.js"
     ],
     testPathIgnorePatterns: [
         "/node_modules",
+        // "/__tests__/*.js",
         "/__tests__/units/TestCase*"
     ]
 }
