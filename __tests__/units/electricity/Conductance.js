bb761f2d15f45c5c2c9f2e564c9c16f374069988
Eldelshell <alejandroayuso@gmail.com>
2018-01-27 16:23:29 +0100

    Add electric Conductance

---
+++ __tests__/units/electricity/Conductance.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import Conductance from '../../../lib/units/electricity/Conductance';
+import TestCase from '../TestCase';
+
+const tests = [{
+    from: 'S', to: 'kS', values: [{ a: 1, b: 0.001 }]
+},{
+    from: 'S', to: 'mS', values: [{ a: 1, b: 1000 }]
+},{
+    from: 'S', to: 'µS', values: [{ a: 1, b: 1000000 }]
+},{
+    from: 'MS', to: 'abS', values: [{ a: 1, b: 0.001 }]
+},{
+    from: 'S', to: 'statS', values: [{ a: 0.00001, b: 8990380 }]
+},{
+    from: 'A/V', to: 'S', values: [{ a: 1, b: 1 }]
+}];
+
+TestCase.units(Conductance, tests);
