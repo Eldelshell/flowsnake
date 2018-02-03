d093098442f67eefa2eca71a73614763241c1fec
Eldelshell <alejandroayuso@gmail.com>
2018-02-04 00:10:40 +0100

    Split tests so it's easier to test single folders.

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
     verbose: false,
     testMatch: [
-        "**/__tests__/**/*.js"
-        // "**/__tests__/**/units/distance/Metric.js"
+        // "**/__tests__/**/*.js"
+        "**/__tests__/**/calculators/**/*.js",
+        "**/__tests__/**/geometry/**/*.js",
+        "**/__tests__/**/units/**/*.js",
+        "**/__tests__/**/Flowsnake.js"
     ],
     testPathIgnorePatterns: [
         "/node_modules",
-        // "/__tests__/*.js",
+        "/__tests__/*.js",
         "/__tests__/units/TestCase*"
     ]
 }
