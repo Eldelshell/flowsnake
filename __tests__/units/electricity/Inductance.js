80fc5e20290ceead1a3c1fe99843c1eb5d718f3a
Eldelshell <alejandroayuso@gmail.com>
2018-02-08 18:49:07 +0100

    Add Electric Inductance

---
+++ __tests__/units/electricity/Inductance.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import Inductance from '../../../lib/units/electricity/Inductance';
+import TestCase from '../TestCase';
+
+const tests = [{
+    from: 'H', to: 'kH', values: [{ a: 1, b: 0.001 }]
+},{
+    from: 'H', to: 'mH', values: [{ a: 1, b: 1000 }]
+},{
+    from: 'H', to: 'µH', values: [{ a: 1, b: 1000000 }]
+},{
+    from: 'MH', to: 'abH', values: [{ a: 1, b: 0.001 }]
+},{
+    from: 'H', to: 'statH', values: [{ a: 0.00001, b: 8990380 }]
+},{
+    from: 'Wb/A', to: 'H', values: [{ a: 1, b: 1 }]
+}];
+
+TestCase.units(Inductance, tests);
